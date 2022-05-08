c588ea5386f80b25a3667c741b9a0eb9c300b1b7
Francine-Claire <100173959+Francine-Claire@users.noreply.github.com>
2022-05-08 11:25:20 +0800

    Add files via upload

--- game.py
+++ game.py
@@ -1,38 +1,27 @@
-import random
-import cardDeck
-import card.py
-import player
-import pile
+"""
+CPE106L_B1 - PROJECT (GAME OF WAR)
 
-def play_war(pile):
-    player_a = pile[:len(pile)/2]
-    player_b = pile[len(pile)/2:]
-    a_stash = []
-    b_stash = []
+=======GROUP 3=======
+CHUA, Vhal Pearson H.
+FONSECA, Robne Kyle L.
+GOPOLE, Khyle Matthew P.
+PUNZALAN, Francine Claire T.
 
-    round = 1
-    while a_cards and b_cards:
-            a_card = a_cards.pop()
-            b_card = b_cards.pop()
 
-            if a_card == b_card:
-                a_stash.extend([a_card]+a_cards[-3:])
-                a_cards = a_cards[:-3]
-                a_cards.append(a_stash.pop())
+MAIN PYTHON SOURCE FILE
+"""
 
-                b_stash.extend([b_card]+b_cards[-3:])
-                b_cards = b_cards[:-3]
-                b_cards.append(b_stash.pop())
-            elif a_card > b_card:
-                a_cards = [a_card, b_card] + a_stash + b_stash + a_cards
-                a_stash = []
-                b_stash = []
-            elif b_card > a_card:
-                b_cards = [b_card, a_card] + b_stash + a_stash + b_cards
-                a_stash = []
-                b_stash = []
+from Logic import * #import from Logic.py to use Logic class
 
-            print (round, len(a_cards), len(a_stash), len(b_cards), len(b_stash))
-            round += 1
-    if __name__ == '__main__':
-        play_war(pile)
+def game():
+    print("========WELCOME TO GAME OF WAR========\n\nHERE ARE THE PLAYERS AND THEIR RESPECTIVE CARDS\n")
+    logic = Logic(['Vhal', 'Francine', 'Robne', 'Khyle'])
+    logic.deal_cards()
+    logic.play_all()
+
+def print_underline(string, line):
+    print('\n{}\n{}'.format(string, line * len(string)))
+
+
+if __name__ == '__main__':
+    game()
